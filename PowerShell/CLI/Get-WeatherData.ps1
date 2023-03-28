075e66bea36e03f3c538e108f2842d2c8c97f4de
Serge Zuidinga <serge.zuidinga@microsoft.com>
2023-03-28 15:31:49 +0200

    v0.1 of the PowelShell scipt

---
+++ PowerShell/CLI/Get-WeatherData.ps1
@@ -0,0 +1,44 @@
+<#
+  .SYNOPSIS
+  Gets weather data for one or more specified cities.
+
+  .DESCRIPTION
+  This PowerShell script queries the OpenWeather API for one or more specified cities and gets the weather data for those cities.
+
+  .EXAMPLE
+  PS C:\> Get-WeatherData.ps1 -ApiKey jlj3f34lkjf34lkf43lkfj3kl24flk34 -CityIds 2759887
+
+  .EXAMPLE
+  PS C:\> Get-WeatherData.ps1 -ApiKey jlj3f34lkjf34lkf43lkfj3kl24flk34 -CityIds 2759887,2759794
+
+  .EXAMPLE
+  PS C:\> Get-WeatherData.ps1 -ApiKey jlj3f34lkjf34lkf43lkfj3kl24flk34 -CityIds 2759887,2759794 -Unit Metric
+
+  .EXAMPLE
+  PS C:\> Get-WeatherData.ps1 -ApiKey jlj3f34lkjf34lkf43lkfj3kl24flk34 -CityIds 2759887,2759794 -Unit Kelvin
+
+  .LINK
+  More information can be found at https://github.com/sezuidin/OpenWeather
+#>
+
+[CmdletBinding()]
+param (
+  [Parameter(HelpMessage='You can get an API token from https://openweathermap.org/')]
+  [String]
+  $ApiKey = $(throw 'Provide an Api Key to authorize against https://api.openweathermap.org'),
+
+  [Parameter(HelpMessage='You can get any city id from https://openweathermap.org/')]
+  [String[]]
+  $CityIds = $(throw 'Enter at least one city id to collect weather information for'),
+
+  [Parameter(HelpMessage='Select the appropriate unit. Available units are Imperial, Kelvin and Metric. Default is Metric.')]
+  [ValidateSet('Imperial', 'Kelvin', 'Metric')]
+  [String]
+  $Unit = 'Metric'
+)
+
+foreach ($cityId in $CityIds) {
+  $cityWeatherDetails = Invoke-RestMethod -Method Get -Uri "https://api.openweathermap.org/data/2.5/weather?id=$cityId&APPID=$ApiKey&units=$Unit"
+
+  $cityWeatherDetails
+}
